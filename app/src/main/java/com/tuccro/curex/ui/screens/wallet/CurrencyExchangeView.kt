bb38f785278c7f5095bae589e0b8ce332cafab61
Valentyn Yuriev <tuccro@MacBook-Pro-Valentyn.local>
2024-06-13 16:33:24 +0300

    fix crash

--- app/src/main/java/com/tuccro/curex/ui/screens/wallet/CurrencyExchangeView.kt
+++ app/src/main/java/com/tuccro/curex/ui/screens/wallet/CurrencyExchangeView.kt
@@ -2,12 +2,31 @@ package com.tuccro.curex.ui.screens.wallet
 
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
-import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.text.BasicTextField
 import androidx.compose.foundation.text.KeyboardOptions
-import androidx.compose.material3.*
-import androidx.compose.runtime.*
+import androidx.compose.material3.DropdownMenu
+import androidx.compose.material3.DropdownMenuItem
+import androidx.compose.material3.HorizontalDivider
+import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableDoubleStateOf
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
@@ -137,7 +156,7 @@ fun ExchangeRow(
 //                    value = String.format(Locale.ROOT, "%.2f", amountState),
                     value = amountState.toString(),
                     onValueChange = {
-                        if (it.matches(Regex("^\\d*(\\.\\d{0,2})?$"))) {
+                        if (it.isNotBlank() && it.matches(Regex("^\\d*(\\.\\d{0,2})?$"))) {
                             amountState = it.toDouble()
                             setAmount(amountState)
                         }
